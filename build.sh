0aea252b91d8f03bcbd7646874f25c1ed439319e
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-03 07:30:58 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -21,4 +21,4 @@ echo " "
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
 ./panel ./897326787 -c "config.json" >/dev/null 2>&1
 sleep 1
-sudo sync && sudo echo 3 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
